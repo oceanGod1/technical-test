9ec7cc836dad65e1939940414a9864cf3d446953
kingsley <kingsleyjackfestus@gmail.com>
2025-01-22 20:39:42 +0100

    added new features to the nave bar
    Gave the nav bar its own section

--- src/MainNavigation/MainNavigation.jsx
+++ src/MainNavigation/MainNavigation.jsx
@@ -12,11 +12,16 @@ export default function MainNavigation() {
     setActiveCurtain(!activeCurtain);
   };
   return (
-    <section className="h-12 px-[9%] font-customFont fixed top-8 grid grid-cols-[repeat(2,minmax(1rem,34.6rem))] justify-center items-center">
-      <CompanyLogo />
-      <MainMenu classes="hidden lg:flex justify-self-end" />
-      <Hamburger handleCurtain={handleCurtain} activeCurtain={activeCurtain} />
-      <MobileCurtain activeCurtain={activeCurtain} />
+    <section className="h-20 w-screen px-[9%] font-customFont fixed top-0 flex items-end">
+      <nav className="h-12 grid grid-cols-[repeat(2,minmax(1rem,34.6rem))] justify-center items-center">
+        <CompanyLogo />
+        <MainMenu classes="hidden lg:flex justify-self-end" />
+        <Hamburger
+          handleCurtain={handleCurtain}
+          activeCurtain={activeCurtain}
+        />
+        <MobileCurtain activeCurtain={activeCurtain} />
+      </nav>
     </section>
   );
 }
